8189fb9b5c132c3a25f7596558d7142fab854e88
Subham Jena <103844138+udiscopotato@users.noreply.github.com>
2023-08-03 17:23:54 +0530

    first commit after project setup

---
+++ src/main/java/com/example/socialmedia/SocialmediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaApiApplication.class, args);
+	}
+
+}
